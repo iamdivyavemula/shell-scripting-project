f4ea12604e8e95db1c496b2eb2830d5d23d936b8
iamdivyavemula <157808338+iamdivyavemula@users.noreply.github.com>
2024-03-17 02:03:32 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+##############
+# About: Print list of users having access to the repo within the organization
+# Input: Organization name repo name
+#
+############################
+
+helper()
 
 # GitHub API URL
 API_URL="https://api.github.com"
@@ -36,6 +43,12 @@ function list_users_with_read_access {
     fi
 }
 
+function helper{
+expected_cmd_args=2
+if[$# -ne $expected_cmd_args]; then
+echo "please execute the script with required command args"
+echo "asd"
+}
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
