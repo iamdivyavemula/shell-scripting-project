64e11caf4014099f821a30c1ec974ef0cfc7ebbc
iamdivyavemula <157808338+iamdivyavemula@users.noreply.github.com>
2024-03-17 02:15:55 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -43,12 +43,12 @@ function list_users_with_read_access {
     fi
 }
 
-function helper{
+function helper {
 expected_cmd_args=2
 if[$# -ne $expected_cmd_args]; then
 echo "please execute the script with required command args"
-echo "asd"
 }
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
